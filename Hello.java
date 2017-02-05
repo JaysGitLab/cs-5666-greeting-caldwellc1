17a75c42f0158a053fad6a55fe56d538cad61eee
Cydney Caldwell <caldwellc1@appstate.edu>
2017-02-04 23:00:55 -0500

    prog name fin

--- Hello.java
+++ Hello.java
@@ -3,6 +3,10 @@ public class Hello {
         if(args.length == 1){
             System.out.println("Hello, " + args[0].substring(0,1).toUpperCase() + args[0].substring(1));
         }
+        else if(args.length == 3 && args[0].equals("-me")){
+            System.out.println("Hello, " + args[2].substring(0,1).toUpperCase() + args[2].substring(1));
+            String programName = args[1];
+        }
         else{
             System.out.println("Hello, Cydney");
         }
